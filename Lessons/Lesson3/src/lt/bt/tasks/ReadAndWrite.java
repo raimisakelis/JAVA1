4a4fc26d61f905e4fabaf738cbce395276aeee05
DESKTOP-21PPQAG\Asus <raimisakelis@gmail.com>
2018-11-16 12:30:24 +0200

    make counting system

--- Lessons/Lesson3/src/lt/bt/tasks/ReadAndWrite.java
+++ Lessons/Lesson3/src/lt/bt/tasks/ReadAndWrite.java
@@ -11,24 +11,54 @@ public class ReadAndWrite {
 
     public static void main (String[] args) throws IOException {
 
-//        //read from file
+        //read from file
 
         String filePath = "res\\txt\\demo\\sample1.txt";
         File file = new File(filePath);
         BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //write to file
+
+        String fileWrite = "res\\txt\\demo\\sample2.txt";
+        FileWriter fw = new FileWriter(fileWrite);
+        BufferedWriter bw = new BufferedWriter(fw);
+
         String st;
         while ((st = br.readLine()) != null) {
-            System.out.println(st);
-            String fileName = "res\\txt\\demo\\sample2.txt";
-            String content = st;
-            FileWriter fw = new FileWriter(fileName);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(content);
-            System.out.println("Done");
-            bw.close();
-            fw.close();
+            bw.write(String.valueOf(st) + "\r\n"); // "\r\n" - newLine
         }
+        System.out.println("Done");
         br.close();
-    }
+        bw.close();
+        fw.close();
 
+
+
+
+
+        //code below just to understand how it work
+
+//        //read from file
+//
+//        String kopijuojamofailokelias = "res\\txt\\demo\\sample1.txt";
+//        File naujasfailas = new File(kopijuojamofailokelias);
+//        BufferedReader bskaitytuvas = new BufferedReader(new FileReader(naujasfailas));
+//
+//        //write to file
+//
+//        String irasomofailokelias = "res\\txt\\demo\\sample2.txt";
+//        FileWriter naujasfailas2 = new FileWriter(irasomofailokelias);
+//        BufferedWriter brasytuvas = new BufferedWriter(naujasfailas2);
+//
+//        String st;
+//        while ((st = bskaitytuvas.readLine()) != null) {
+//            brasytuvas.write(String.valueOf(st) + "\r\n"); // "\r\n" - newLine
+//        }
+//        System.out.println("Done");
+//        bskaitytuvas.close();
+//        brasytuvas.close();
+//        naujasfailas2.close();
+
+
+    }
 }
---
+++ Lessons/Lesson3/src/lt/bt/tasks/ReadCountAndWrite.java
@@ -0,0 +1,60 @@
+package lt.bt.tasks;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class ReadCountAndWrite {
+
+    public static void main (String[] args) throws IOException {
+
+        //create new numbers file
+
+        String filePath = "res\\txt\\demo\\numbersfile.txt";
+        File filenumbers = new File(filePath);
+        FileWriter fw = new FileWriter(filenumbers);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        int quantitynumbers = 200;
+        int number = 1;
+        for (int i = 0; i < quantitynumbers; i++) {
+            bw.write(String.valueOf(number++) + "\r\n");
+        }
+        System.out.println("Done");
+        bw.close();
+        fw.close();
+
+        //read from numbers file and count
+
+        String filePath2 = "res\\txt\\demo\\numbersfile.txt";
+        BufferedReader br = new BufferedReader(new FileReader(filePath2));
+        String st;
+        int sum = 0;
+        int quantity = 0;
+        while ((st = br.readLine()) != null) {
+            sum += Integer.parseInt(String.valueOf(st));
+            quantity++;
+        }
+        double avarage = Double.valueOf(sum)/Double.valueOf(quantity);
+        br.close();
+        System.out.println("Faile esanciu skaiciu suma: " + sum);
+        System.out.println("Faile esanciu duomenų kiekis: " + quantity);
+        System.out.println("Faile esanciu duomenų vidurkis: " + avarage);
+
+        //write results to new resultsfile
+
+        String filePath3 = "res\\txt\\demo\\resultsfile.txt";
+        File fileresults = new File(filePath3);
+        FileWriter fw3 = new FileWriter(fileresults);
+        BufferedWriter bw3 = new BufferedWriter(fw3);
+        bw3.write(String.valueOf(avarage + "\r\n"));
+        System.out.println("Task Done");
+        bw3.close();
+        fw3.close();
+
+
+    }
+}
