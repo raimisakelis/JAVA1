5b3c983cacb8e1de7d59a83866106915357c4fe5
DESKTOP-NC2SI5R\PC <raimisakelis@gmail.com>
2018-11-19 21:46:56 +0200

    task 1 and 2 solve class RemoveItemsSmallerThanAverage
    task 3 and 4 solve class AverageOfElements

--- Lessons/Lesson4/src/lt/bt/tasks/AverageOfElements.java
+++ Lessons/Lesson4/src/lt/bt/tasks/AverageOfElements.java
@@ -8,21 +8,83 @@ public class AverageOfElements {
 
         public static void main (String[] args) throws IOException {
 
+            //tasks 3 - negative elements of array move to new array
+
             String filePath2 = "C:\\Program Files (x86)\\Ampps\\www\\JAVA1\\Lessons\\Lesson4\\res\\txt\\demo\\elementsofarray";
             int[] data = readFiles(filePath2);
-            System.out.println(Arrays.toString(data));
+            System.out.println("Pradinis masyvas " + Arrays.toString(data));
 
-           int sum = 0;
-           for (int j = 0; j < data.length; j++) {
-              sum += data[j];
-          }
 
-          double average = sum / data.length;
+        //how many negative elements are in the array
 
-           System.out.println("Masyvo elementu suma: " + sum);
-          System.out.println("Elementu kiekis masyve: " + data.length);
-            System.out.println("Masyvo elementu vidurkis: " + average);
+            int negativeQuantity = 0;
+
+            for (int j = 0; j < data.length; j++) {
+
+                if (data[j] < 0) {
+                    negativeQuantity++;
+
+                }
+            }
+
+
+            int[] negativeData = new int[negativeQuantity];//new array with length negativeQuantity
+            int[] dataWithoutNegative = new int[data.length - negativeQuantity];//new array without negative elements
+            int m = 0;
+            int n = 0;
+
+            for (int k = 0; k < data.length; k++) {
+                if (data[k] < 0) {
+                    negativeData[m] = data[k];
+                    m++;
+
+                } else {
+                    dataWithoutNegative[n] = data[k];
+                    n++;
+                }
+            }
+
+            System.out.println("Neigiamu skaiciu masyvas " + Arrays.toString(negativeData));
+//            System.out.println("Likusiu skaiciu masyvas " + Arrays.toString(dataWithoutNegative));
+
+            data = dataWithoutNegative;
+            System.out.println("Pradinis masyvas be neigiamu skaiciu " + Arrays.toString(data));
+
+
+            //tasks 4 - sort both arrays
+
+            for (int r = 0; r < negativeData.length; r++) {
+                for (int p = r + 1; p < negativeData.length; p++) {
+                    int sk1 = negativeData[r];
+                    int sk2 = negativeData[p];
+                    if (sk1 > sk2) {
+                        negativeData[r] = sk2;
+                        negativeData[p] = sk1;
+                    }
+                }
+
+            }
+
+            System.out.println("Isrikiuotas neigiamu skaiciu masyvas " + Arrays.toString(negativeData));
+
+            for (int a = 0; a < data.length; a++) {
+                for (int b = a + 1; b < data.length; b++) {
+                    int sk3 = data[a];
+                    int sk4 = data[b];
+                    if (sk4 < sk3) {
+                        data[a] = sk4;
+                        data[b] = sk3;
+                    }
+                }
+            }
+
+            System.out.println("Isrikiuotas pradinis masyvas be neigiamu skaiciu " + Arrays.toString(data));
+
+
+
+
         }
+
 
 
         private static int[] readFiles(String filePath2) {
---
+++ Lessons/Lesson4/src/lt/bt/tasks/RemoveItemsSmallerThanAverage.java
@@ -0,0 +1,101 @@
+package lt.bt.tasks;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RemoveItemsSmallerThanAverage {
+
+    public static void main (String[] args) throws IOException {
+
+        //tasks 1  - count average
+
+        String filePath2 = "C:\\Program Files (x86)\\Ampps\\www\\JAVA1\\Lessons\\Lesson4\\res\\txt\\demo\\elementsofarray";
+        int[] data = readFiles(filePath2);
+        System.out.println(Arrays.toString(data));
+
+
+        int sum = 0;
+        for (int j = 0; j < data.length; j++) {
+            sum += data[j];
+        }
+
+        double average = sum / data.length;
+
+        System.out.println("Masyvo elementu suma: " + sum);
+        System.out.println("Elementu kiekis masyve: " + data.length);
+        System.out.println("Masyvo elementu vidurkis: " + average);
+
+
+        //tasks 2 - remove elements smaller than average
+
+        //how much smaller elements (than the average) are in the array
+
+        int smallerQuantity = 0;
+
+        for (int k = 0; k < data.length; k++) {
+
+            if (data[k] < average) {
+                smallerQuantity++;
+
+            }
+        }
+
+        System.out.println("Elementu, mazesniu us vidurki, kiekis: " + smallerQuantity);
+
+        //create new array without elements smaller than average
+
+        int[] smallerData = new int[smallerQuantity];
+        int g = 0;
+
+        for (int l = 0; l < data.length; l++) {
+
+            if (data[l] < average) {
+                smallerData[g] = data[l];
+                g++;
+            }
+
+        }
+
+
+        System.out.println(Arrays.toString(smallerData));
+
+
+
+    }
+
+
+    //code below needed to scan the data in the array
+
+    private static int[] readFiles(String filePath2) {
+
+
+        try {
+
+            // count elements quantity
+            File f = new File(filePath2);
+            Scanner s = new Scanner(f);
+            int ctr = 0;
+            while (s.hasNextInt()) {
+                ctr++;
+                s.nextInt();
+            }
+
+
+            //create new array with length ctr
+            int[] arr = new int[ctr];
+            Scanner s1 = new Scanner(f);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = s1.nextInt();
+            return arr;
+
+        }
+
+        catch (Exception e) {
+            return null;
+        }
+
+    }
+
+}
