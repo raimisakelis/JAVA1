5b0980d0fe477d0f49fbe914714d5efacce19e43
DESKTOP-NC2SI5R\PC <raimisakelis@gmail.com>
2018-11-20 23:29:58 +0200

    include function and method

--- Lessons/Lesson4/src/lt/bt/tasks/AverageOfElements.java
+++ Lessons/Lesson4/src/lt/bt/tasks/AverageOfElements.java
@@ -15,72 +15,120 @@ public class AverageOfElements {
             System.out.println("Pradinis masyvas " + Arrays.toString(data));
 
 
-        //how many negative elements are in the array.
+        //how many negative and positive elements are in the array "data"
+            int negativeQuantity = quantityNegativeElements(data);
+            int positiveQuantity = data.length - negativeQuantity;
 
-            int negativeQuantity = 0;
+        //create negative and positive numbers arrays
 
-            for (int j = 0; j < data.length; j++) {
+            int[] negativeArray = negativeElementsArray(data, negativeQuantity);//new array with length negativeQuantity
+            int[] positiveArray = positiveElementsArray(data, positiveQuantity);//new array without negative elements
+            data = positiveArray; //write positive numbers into initial array "data"
 
-                if (data[j] < 0) {
-                    negativeQuantity++;
-                }
-            }
+        //return results
+            printText("Neigiamu skaiciu masyvas " + Arrays.toString(negativeArray));
+            printText("Pradinis masyvas be neigiamu skaiciu" + Arrays.toString(data));
 
-
-            int[] negativeData = new int[negativeQuantity];//new array with length negativeQuantity
-            int[] dataWithoutNegative = new int[data.length - negativeQuantity];//new array without negative elements
-            int m = 0;
-            int n = 0;
-
-            for (int k = 0; k < data.length; k++) {
-                if (data[k] < 0) {
-                    negativeData[m] = data[k];
-                    m++;
-
-                } else {
-                    dataWithoutNegative[n] = data[k];
-                    n++;
-                }
-            }
-
-            System.out.println("Neigiamu skaiciu masyvas " + Arrays.toString(negativeData));
-//            System.out.println("Likusiu skaiciu masyvas " + Arrays.toString(dataWithoutNegative));
-
-            data = dataWithoutNegative;
-            System.out.println("Pradinis masyvas be neigiamu skaiciu " + Arrays.toString(data));
+        //sort array
+            sortAscendingOrder("Isrikiuotas masyvas didejimo tvarka", negativeArray);
+            sortAscendingOrder("Isrikiuotas masyvas didejimo tvarka", positiveArray);
 
 
-            //tasks 4 - sort both arrays
 
-            for (int r = 0; r < negativeData.length; r++) {
-                for (int p = r + 1; p < negativeData.length; p++) {
-                    int sk1 = negativeData[r];
-                    int sk2 = negativeData[p];
-                    if (sk1 > sk2) {
-                        negativeData[r] = sk2;
-                        negativeData[p] = sk1;
-                    }
-                }
 
-            }
 
-            System.out.println("Isrikiuotas neigiamu skaiciu masyvas " + Arrays.toString(negativeData));
 
-            for (int a = 0; a < data.length; a++) {
-                for (int b = a + 1; b < data.length; b++) {
-                    int sk3 = data[a];
-                    int sk4 = data[b];
-                    if (sk4 < sk3) {
-                        data[a] = sk4;
-                        data[b] = sk3;
-                    }
-                }
-            }
 
-            System.out.println("Isrikiuotas pradinis masyvas be neigiamu skaiciu " + Arrays.toString(data));
+//            //tasks 4 - sort both arrays
+//
+//            for (int r = 0; r < negativeData.length; r++) {
+////                for (int p = r + 1; p < negativeData.length; p++) {
+////                    int sk1 = negativeData[r];
+////                    int sk2 = negativeData[p];
+////                    if (sk1 > sk2) {
+////                        negativeData[r] = sk2;
+////                        negativeData[p] = sk1;
+////                    }
+////                }
+////
+////            }
+//
+//            System.out.println("Isrikiuotas neigiamu skaiciu masyvas " + Arrays.toString(negativeData));
+//
+//            for (int a = 0; a < data.length; a++) {
+//                for (int b = a + 1; b < data.length; b++) {
+//                    int sk3 = data[a];
+//                    int sk4 = data[b];
+//                    if (sk4 < sk3) {
+//                        data[a] = sk4;
+//                        data[b] = sk3;
+//                    }
+//                }
+//            }
+//
+//            System.out.println("Isrikiuotas pradinis masyvas be neigiamu skaiciu " + Arrays.toString(data));
 
         }
 
+
+
+    //return text
+        private static void printText(String text) {System.out.println(text);}
+
+        //return text and array
+        private static void  sortAscendingOrder(String text, int[] array) {
+            for (int r = 0; r < array.length; r++) {
+                for (int p = r + 1; p < array.length; p++) {
+                    int sk1 = array[r];
+                    int sk2 = array[p];
+                    if (sk1 > sk2) {
+                        array[r] = sk2;
+                        array[p] = sk1;
+                    }
+                }
+            }
+
+            System.out.println(text + " " +  Arrays.toString(array));
+        }
+
+        //this function count negative elements of the array
+        private static int quantityNegativeElements(int[] array) {
+            int quantity = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] < 0) {
+                    quantity++;
+                }
+            }
+
+            return quantity;
+        }
+
+
+        //this function writing negative elements into new array
+        private static int[] negativeElementsArray(int[] initialArray, int newArrayLength) {
+            int m = 0;
+            int[] negativeArray = new int[newArrayLength];
+            for (int k = 0; k < initialArray.length; k++) {
+                if (initialArray[k] < 0) {
+                    negativeArray[m] = initialArray[k];
+                    m++;
+                }
+            }
+                return negativeArray;
+        }
+
+        //this function writing positive elements into new array
+        private static int[] positiveElementsArray(int[] initialArray, int newArrayLength) {
+            int n = 0;
+            int[] positiveArray = new int[newArrayLength];
+            for (int k = 0; k < initialArray.length; k++) {
+                if (initialArray[k] >= 0) {
+                    positiveArray[n] = initialArray[k];
+                    n++;
+                }
+            }
+            return positiveArray;
+        }
 
 
         private static int[] readFiles(String filePath2) {
